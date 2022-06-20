63bce81e13c9164aba54388131b4e04eaf9c4f63
Valentin Repetto <4385@holbertonschool.com>
2022-06-20 15:49:04 -0300

    no funca

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const conexion = require('./db/connection')
 const crud = require('./controllers/crud');
+const Connection = require('mysql/lib/Connection');
 
 
 router.get('/', (req,res) => {
@@ -24,10 +25,22 @@ router.get('/edit/:id', (req,res)=>{
         if (error) {
             throw error;
         }else{            
-            res.render('edit.ejs', {user:results[0]});            
+            res.render('edit', {user:results[0]});            
         }        
     });
 });
+
+router.get('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    conexion.query('DELETE FROM usuarios WHERE id = ?',[id], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{           
+            res.redirect('/');         
+        }
+    })
+});
+
 
 router.post('/save', crud.save)
 
